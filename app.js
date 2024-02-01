58d196db94960726c95ebcaa83cdc4dcb2e16960
Deus lo Vult <92962731+Lostovayne@users.noreply.github.com>
2024-02-01 04:40:19 -0300

    updated

--- app.js
+++ app.js
@@ -1,23 +1,20 @@
-import express from 'express'
-import { corsMiddleware } from './middlewares/cors.js'
-import movieRouter from './routes/movies.js'
+import express from "express";
+import { corsMiddleware } from "./middlewares/cors.js";
+import movieRouter from "./routes/movies.js";
 
-const app = express()
+const app = express();
 
-app.disable('x-powered-by')
-app.use(express.json())
-app.get('/', (req, res) => {
-  res.json({ message: 'Hello World!' })
-})
-app.use('/movies', movieRouter)
-app.use(corsMiddleware()) // Cors solucionado debe ir debajo de las rutas
+app.disable("x-powered-by");
+app.use(express.json());
+app.use("/movies", movieRouter);
+app.use(corsMiddleware()); // Cors solucionado debe ir debajo de las rutas
 // Listado de Direcciones de acceso permitidas para los CORS
 // const ACCEPTED_ORIGINS = [
 //     "https://api-movies-midu.vercel.app",
 // ];
 
-const PORT = process.env.PORT || 1234
+const PORT = process.env.PORT || 1234;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+    console.log(`Server running on port ${PORT}`);
+});
