a59a0fdd28063f0792721ac795c51e4c399e67df
Deus lo Vult <92962731+Lostovayne@users.noreply.github.com>
2024-02-01 05:36:09 -0300

    add comment in models

--- models/movie.js
+++ models/movie.js
@@ -1,42 +1,43 @@
-import crypto from 'crypto'
-import { readJSON } from '../utils.js'
-const movies = readJSON('./movies.json')
+import crypto from "crypto";
+import { readJSON } from "../utils.js";
+const movies = readJSON("./movies.json");
 
+// Modelos que pueden conectarse a la base de datos
 export class MovieModel {
-  static async getAll ({ genre }) {
-    if (genre) {
-      const filteredMovies = movies.filter((movie) =>
-        movie.genre.some((g) => g.toLocaleLowerCase() === genre.toLocaleLowerCase())
-      )
-      return filteredMovies
+    static async getAll({ genre }) {
+        if (genre) {
+            const filteredMovies = movies.filter((movie) =>
+                movie.genre.some((g) => g.toLocaleLowerCase() === genre.toLocaleLowerCase())
+            );
+            return filteredMovies;
+        }
+        return movies;
     }
-    return movies
-  }
 
-  static async getById ({ id }) {
-    return movies.find((movie) => movie.id === id)
-  }
-
-  static async create ({ input }) {
-    const newMovie = { id: crypto.randomUUID(), ...input } // =>  Agregar en la BD
-    movies.push(newMovie)
-    return newMovie // => Actualiar la cache del cliente
-  }
-
-  static async delete ({ id }) {
-    // Peticion a la base de datos
-    const movieIndex = movies.findIndex((movie) => movie.id === id)
-    if (movieIndex === -1) return false
-    movies.splice(movieIndex, 1)
-    return true
-  }
-
-  static async update ({ id, input }) {
-    const movieIndex = movies.findIndex((movie) => movie.id === id)
-    if (movieIndex === -1) return false
-    movies[movieIndex] = { ...movies[movieIndex], ...input }
-    return {
-      ...movies[movieIndex]
+    static async getById({ id }) {
+        return movies.find((movie) => movie.id === id);
     }
-  }
+
+    static async create({ input }) {
+        const newMovie = { id: crypto.randomUUID(), ...input }; // =>  Agregar en la BD
+        movies.push(newMovie);
+        return newMovie; // => Actualiar la cache del cliente
+    }
+
+    static async delete({ id }) {
+        // Peticion a la base de datos
+        const movieIndex = movies.findIndex((movie) => movie.id === id);
+        if (movieIndex === -1) return false;
+        movies.splice(movieIndex, 1);
+        return true;
+    }
+
+    static async update({ id, input }) {
+        const movieIndex = movies.findIndex((movie) => movie.id === id);
+        if (movieIndex === -1) return false;
+        movies[movieIndex] = { ...movies[movieIndex], ...input };
+        return {
+            ...movies[movieIndex],
+        };
+    }
 }
